3f66b30bd0108af69c00b8ba212871bf4951d229
adeelehsan <adeel.ehsan@arbisoft.com>
2021-05-27 16:14:34 +0500

    design updated

--- src/alerts/logistration-alert/AccountActivationAlert.jsx
+++ src/alerts/logistration-alert/AccountActivationAlert.jsx
@@ -2,15 +2,22 @@ import React, { useState } from 'react';
 import Cookies from 'js-cookie';
 import { getConfig } from '@edx/frontend-platform';
 import { getAuthenticatedUser } from '@edx/frontend-platform/auth';
-import { AlertModal, Button } from '@edx/paragon';
+import { AlertModal, Button, Spinner, Icon } from '@edx/paragon';
+import { Check } from '@edx/paragon/icons';
 import { FormattedMessage, injectIntl } from '@edx/frontend-platform/i18n';
 import { sendActivationEmail } from '../../courseware/data';
 
 function AccountActivationAlert() {
   const [showModal, setShowModal] = useState(false);
+  const [showSpinner, setShowSpinner] = useState(false);
+  const [showCheck, setShowCheck] = useState(false);
   const handleOnClick = () => {
-    sendActivationEmail();
-    setShowModal(false);
+    setShowSpinner(true);
+    setShowCheck(false);
+    sendActivationEmail().then(() => {
+      setShowSpinner(false);
+      setShowCheck(true);
+    });
   };
 
   const showAccountActivationAlert = Cookies.get('show-account-activation-popup');
@@ -50,34 +57,63 @@ function AccountActivationAlert() {
     </Button>
   );
 
+  const children = () => {
+    let bodyContent = null;
+    const message = (
+      <FormattedMessage
+        id="account-activation.alert.message"
+        defaultMessage="We sent an email to {boldEmail} with a link to activate your account. Can’t find it? Check your spam folder or
+        {sendEmailTag}."
+        description="Message for account activation alert which is shown after the registration"
+        values={{
+          boldEmail: <b>{getAuthenticatedUser().email}</b>,
+          sendEmailTag: (
+          // eslint-disable-next-line jsx-a11y/anchor-is-valid
+            <a href="#" role="button" onClick={handleOnClick}>
+              <FormattedMessage
+                id="account-activation.resend.link"
+                defaultMessage="resend the email"
+                description="Message for resend link in account activation alert which is shown after the registration"
+              />
+            </a>
+          ),
+        }}
+      />
+    );
+    bodyContent = (
+      <p>
+        {message}
+      </p>
+    );
+
+    if (showCheck === false && showSpinner === true) {
+      bodyContent = (
+        <p>
+          {message}
+          <Spinner animation="border" variant="primary" size="sm" />
+        </p>
+      );
+    }
+
+    if (showCheck === true && showSpinner === false) {
+      bodyContent = (
+        <p>
+          {message}
+          <Icon src={Check} className="text-success account-activation-check" />
+        </p>
+      );
+    }
+    return bodyContent;
+  };
+
   return (
     <AlertModal
       isOpen={showModal}
       title={title}
       footerNode={button}
-      onClose={() => ({})}
+      close={() => ({})}
     >
-      <p>
-        <FormattedMessage
-          id="account-activation.alert.message"
-          defaultMessage="We sent an email to {boldEmail} with a link to activate your account. Can’t find it? Check your spam folder or
-          {sendEmailTag}."
-          description="Message for account activation alert which is shown after the registration"
-          values={{
-            boldEmail: <b>{getAuthenticatedUser().email}</b>,
-            sendEmailTag: (
-            // eslint-disable-next-line jsx-a11y/anchor-is-valid
-              <a href="#" role="button" onClick={handleOnClick}>
-                <FormattedMessage
-                  id="account-activation.resend.link"
-                  defaultMessage="resend the email"
-                  description="Message for resend link in account activation alert which is shown after the registration"
-                />
-              </a>
-            ),
-          }}
-        />
-      </p>
+      {children()}
     </AlertModal>
   );
 }
